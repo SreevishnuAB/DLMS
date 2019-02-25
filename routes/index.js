d650946edc0cecfc2679a9525f393626dfac8aa1
Sreevishnu <sreeab28@gmail.com>
2019-02-25 15:30:06 +0530

    Added registration route to index, modified index view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
+router.get('/register',(req,res,next)=>{
+  res.send("Registration");
+});
 module.exports = router;
