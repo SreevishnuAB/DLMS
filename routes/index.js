71bd51a2ff7216f65ed665e92605f1668aa4e098
SreevishnuAB <sreeab28@gmail.com>
2019-03-01 15:43:44 +0530

    Implementing user authentication

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(express.json())
+router.use(express.urlencoded({extended:false}));
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index',{title: 'DLMS',auth: false});
 });
 router.post('/login',(req,res,next)=>{
-  
+  res.send("Hello");
 });
 module.exports = router;
