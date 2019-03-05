295d7b13138df14377304050c917814fa2acfb7d
Sreevishnu <sreeab28@gmail.com>
2019-03-05 13:14:32 +0530

    Minor changes, removing unused packages

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(express.json())
-router.use(express.urlencoded({extended:false}));
+//router.use(express.json());
+//router.use(express.urlencoded({extended:false}));
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index',{title: 'DLMS',auth: false});
 });
 router.post('/login',(req,res,next)=>{
-  res.send("Hello");
+//  res.send(req.body.user);
 });
 module.exports = router;
