27e4ecc7743309fcbe6aab09d0a6eaf9ea50f215
Sreevishnu <sreeab28@gmail.com>
2019-03-05 15:38:00 +0530

    Adding bcrypt, implementing user authentication

---
+++ public/scripts/jsscripts.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('.formelement').val('');
+});
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var Sequelize = require('sequelize');
 
-//router.use(express.json());
-//router.use(express.urlencoded({extended:false}));
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index',{title: 'DLMS',auth: false});
 });
 router.post('/login',(req,res,next)=>{
-//  res.send(req.body.user);
+  res.send(req.body.user);
+  console.log(req.body.user);
 });
 module.exports = router;
